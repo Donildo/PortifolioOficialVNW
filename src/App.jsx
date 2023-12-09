6a606cde6a0ac9e3883259400279f98da877cb2b
Donildo <donildobrito307@gmail.com>
2023-12-09 09:41:59 -0400

    portifolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Heroi } from "./Components/Heroi/Heroi";
 import { Sobre } from "./Components/Sobre/Sobre";
+import { Experiencia } from "./Components/Experiencia/Experiencia";
 
 function App() {
     return(
@@ -9,6 +10,7 @@ function App() {
             <Navbar/>
             <Heroi/>
             <Sobre/>
+            <Experiencia/>
         </div>
     );
 }
---
+++ src/Components/Experiencia/Experiencia.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Experiencia.module.css"
+
+export const Experiencia = () => {
+    return (
+        <section className={styles.container} id="Experiencia">
+            <h2 className={styles.title}>Experienica</h2>
+            <div className={styles.content}>
+                <div className={styles.skills}>
+                    <div className={styles.skill}>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
