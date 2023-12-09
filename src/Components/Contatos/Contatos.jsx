913522721cf14144a932354813e3d4a36f68f82e
Donildo <donildobrito307@gmail.com>
2023-12-09 15:41:55 -0400

    portifolio

---
+++ src/Components/Contatos/Contatos.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from ".Contatos.modules.css"
+
+export const Contatos = () => {
+    return(
+        <footer id="contatos"className={styles.container}>
+            <div className={styles.text}>
+                <h2>Contatos</h2>
+                <p>Faça seu orçamento</p>
+                <ul className={styles.links}>
+                    <li className={styles.link}>
+                        <img src="" alt="" />
+                        <a href="">devfrontdonildo@gmail.com</a>
+                    </li>
+                    <li className={styles.link}>
+                        <img src="" alt="" />
+                        <a href="">Linkedin.com</a>
+                    </li>
+                    <li className={styles.link}>
+                        <img src="" alt="" />
+                        <a href="">github.com</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
